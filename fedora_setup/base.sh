4f725a139cba884bda4df85d73815beb5587cc54
Rafael Benvindo <rafaelbenvindo@gmail.com>
2025-04-05 00:44:37 -0300

    Add Vagrantfile
    Modify base.sh:
    Remove --nogpgcheck from terra-release install and add key url;
    install flatpak before repo add;
    change dnf config-manager syntax for docker.

--- fedora_setup/base.sh
+++ fedora_setup/base.sh
@@ -1,9 +1,9 @@
 sudo setenforce 0
 sudo echo -e "\nmax_parallel_downloads=10\nfastestmirror=true" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+sudo dnf install -y --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' --setopt='terra.gpgkey=https://repos.fyralabs.com/terra41/key.asc' terra-release
+sudo dnf install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf update -y --refresh
 sudo dnf upgrade -y --refresh
-sudo 
